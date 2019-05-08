cb3e2b0a2db25789a0ca0c0910b7cbd1ea5d3d09
Luciano Ropero <lropero@janis.lan>
2019-05-08 02:26:12 -0300

    npm support

--- README.md
+++ README.md
@@ -1,16 +1,17 @@
 # Upgradeps
-Command-line utility to update all packages in package.json to latest version, potentially upgrading across major versions.
+Command-line utility to update all packages in package.json to latest version, potentially upgrading across major versions. Uses yarn when available, npm otherwise. Inspired by [yarn-upgrade-all](https://github.com/tylerlong/yarn-upgrade-all#readme).
 
 ### Installation
+yarn
 ```sh
-yarn add --dev upgradeps
+yarn add upgradeps --dev
+```
+npm
+```sh
+npm install upgradeps --save-dev
 ```
 
 ### Usage
 ```sh
-yarn run upgradeps
+npx upgradeps
 ```
-If updates, run `rm -f ./yarn.lock && rm -rf ./node_modules && yarn` and commit `package.json` and `yarn.lock`.
-
-### Philosophy
-I believe that dependencies should be updated at the beginning of every sprint and refactor any breaking changes. For more information on why this is a good practice watch [this](https://www.youtube.com/watch?v=dQw4w9WgXcQ).
--- cli.js
+++ cli.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 const chalk = require('chalk')
+const commandExistsSync = require('command-exists').sync
 const fs = require('fs')
 const path = require('path')
 const { execSync } = require('child_process')
@@ -8,20 +9,29 @@ const pckg = path.resolve(process.cwd(), 'package.json')
 if (!fs.existsSync(pckg)) {
   process.exit(1)
 }
-const { dependencies = {}, devDependencies = {}, peerDependencies = {} } = require(pckg)
+const { dependencies = {}, devDependencies = {} } = require(pckg)
 const { version } = require('./package.json')
 
-console.log(chalk.yellow(`Upgradeps v${version}`))
-const lists = { dependencies, devDependencies, peerDependencies }
-const options = { dependencies: '', devDependencies: ' --dev', peerDependencies: ' --peer' }
+const hasYarn = commandExistsSync('yarn')
+console.log(chalk[hasYarn ? 'green' : 'yellow'](`Upgradeps v${version}`))
+const getCommand = (which, args) => {
+  switch (which) {
+    case 1: return hasYarn ? `yarn info ${args.item} version` : `npm view ${args.item} version`
+    case 2: return hasYarn ? `yarn remove ${args.item} && yarn add ${args.item}${args.option}` : `npm uninstall ${args.item} && npm install ${args.item}${args.option}`
+  }
+}
+const lists = { dependencies, devDependencies }
+const options = hasYarn
+  ? { dependencies: '', devDependencies: ' --dev' }
+  : { dependencies: ' --save', devDependencies: ' --save-dev' }
 Object.keys(lists).map((group) => {
   console.log(chalk.yellow(`…${group}`))
   Object.keys(lists[group]).map((item) => {
     try {
       const current = lists[group][item].replace(/[\^~]/, '').trim()
-      const latest = execSync(`yarn info ${item} version`, { stdio: [] }).toString().trim()
+      const latest = execSync(getCommand(1, { item }), { stdio: [] }).toString().trim()
       if (current !== latest) {
-        execSync(`yarn remove ${item} && yarn add${options[group]} ${item}`, { stdio: [] })
+        execSync(getCommand(2, { item, option: options[group] }), { stdio: [] })
         console.log(`${chalk.cyan(item)} ${chalk.green('✔')} ${current + chalk.yellow(' ➡ ') + latest}`)
       }
     } catch (error) {
