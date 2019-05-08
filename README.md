2e619ef71d3f502490216d1fbc439f25ce0f3ed8
Luciano Ropero <lropero@janis.lan>
2019-05-08 15:51:42 -0300

    Added skip option

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Upgradeps
-Command-line utility to update all packages in package.json to latest version, potentially upgrading across major versions. Uses yarn when available, npm otherwise. Inspired by [yarn-upgrade-all](https://github.com/tylerlong/yarn-upgrade-all#readme).
+Command-line utility to **upgra**de all **dep**endencie**s** in package.json to latest version, potentially updating packages across major versions. Uses yarn when available, npm otherwise. Inspired by [yarn-upgrade-all](https://github.com/tylerlong/yarn-upgrade-all#readme).
 
 ### Installation
 yarn
@@ -14,4 +14,8 @@ npm install upgradeps --save-dev
 ### Usage
 ```sh
 npx upgradeps
+```
+To skip some packages, use `-s` or `--skip` followed by a comma-separated list of packages to ignore:
+```sh
+npx upgradeps -s react,react-dom
 ```
--- cli.js
+++ cli.js
@@ -1,17 +1,21 @@
 #!/usr/bin/env node
 const chalk = require('chalk')
 const commandExistsSync = require('command-exists').sync
+const commander = require('commander')
+const figures = require('figures')
 const fs = require('fs')
 const path = require('path')
 const { execSync } = require('child_process')
 
 const pckg = path.resolve(process.cwd(), 'package.json')
-if (!fs.existsSync(pckg)) {
-  process.exit(1)
-}
+if (!fs.existsSync(pckg)) { process.exit(1) }
 const { dependencies = {}, devDependencies = {} } = require(pckg)
 const { version } = require('./package.json')
 
+commander
+  .version(version, '-v, --version')
+  .option('-s --skip <packages>', 'Skip packages')
+  .parse(process.argv)
 const hasYarn = commandExistsSync('yarn')
 console.log(chalk[hasYarn ? 'green' : 'yellow'](`Upgradeps v${version}`))
 const getCommand = (which, args) => {
@@ -24,6 +28,7 @@ const lists = { dependencies, devDependencies }
 const options = hasYarn
   ? { dependencies: '', devDependencies: ' --dev' }
   : { dependencies: ' --save', devDependencies: ' --save-dev' }
+const skip = (commander.skip || '').split(',')
 Object.keys(lists).map((group) => {
   console.log(chalk.yellow(`…${group}`))
   Object.keys(lists[group]).map((item) => {
@@ -31,11 +36,15 @@ Object.keys(lists).map((group) => {
       const current = lists[group][item].replace(/[\^~]/, '').trim()
       const latest = execSync(getCommand(1, { item }), { stdio: [] }).toString().trim()
       if (current !== latest) {
-        execSync(getCommand(2, { item, option: options[group] }), { stdio: [] })
-        console.log(`${chalk.cyan(item)} ${chalk.green('✔')} ${current + chalk.yellow(' ➡ ') + latest}`)
+        if (skip.includes(item)) {
+          console.log(`${chalk.cyan(item)} ${chalk.yellow(figures.cross)} ${current} ${chalk.yellow(figures.arrowRight)} ${latest}`)
+        } else {
+          execSync(getCommand(2, { item, option: options[group] }), { stdio: [] })
+          console.log(`${chalk.cyan(item)} ${chalk.green(figures.tick)} ${current} ${chalk.yellow(figures.arrowRight)} ${latest}`)
+        }
       }
     } catch (error) {
-      console.log(`${chalk.cyan(item)} ${chalk.red('✘')} ${chalk.yellow(error.toString())}`)
+      console.log(`${chalk.cyan(item)} ${chalk.red(figures.cross)} ${chalk.yellow(error.toString())}`)
     }
   })
 })
