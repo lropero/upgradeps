c062f53618b3faecae8c37681eddd39c6407415d
Luciano Ropero <lropero@gmail.com>
2019-05-30 07:14:11 -0300

    Refactored using libnpm

--- cli.js
+++ cli.js
@@ -1,11 +1,11 @@
 #!/usr/bin/env node
 const chalk = require('chalk')
 const commander = require('commander')
+const libnpm = require('libnpm')
+const path = require('path')
 const { arrowRight, cross, tick } = require('figures')
-const { exec, execSync } = require('child_process')
+const { execSync } = require('child_process')
 const { existsSync, readFileSync, writeFileSync } = require('fs')
-const { forkJoin } = require('rxjs')
-const { resolve } = require('path')
 const { sync: commandExistsSync } = require('command-exists')
 
 const { version } = require('./package.json')
@@ -19,35 +19,53 @@ const errorToString = (error) => {
   return string.charAt(0).toUpperCase() + string.slice(1)
 }
 
-const upgrade = async (options) => {
+const getInfo = () => {
+  const packagePath = path.resolve(process.cwd(), 'package.json')
+  const packageJSON = JSON.parse(readFileSync(packagePath, 'utf8'))
+  const { dependencies = {}, devDependencies = {} } = packageJSON
+  return { deps: { dependencies, devDependencies }, packageJSON, packagePath, pckgs: Object.keys(dependencies).concat(Object.keys(devDependencies)) }
+}
+
+const queryVersions = (pckgs) => new Promise(async (resolve, reject) => {
   try {
-    const packagePath = resolve(process.cwd(), 'package.json')
-    const packageJSON = JSON.parse(readFileSync(packagePath, 'utf8'))
-    const { dependencies = {}, devDependencies = {} } = packageJSON
-    const deps = { dependencies, devDependencies }
-    const pckgs = Object.keys(dependencies).concat(Object.keys(devDependencies))
-    const sources = pckgs.reduce((sources, pckg) => ({
-      ...sources,
-      [pckg]: new Promise((resolve, reject) => {
-        exec(`npm view ${pckg} version`, { stdio: [] }, (error, stdout) => {
-          if (error) {
-            if (error.code === 1) {
-              return resolve(false)
-            }
-            return reject(error)
-          }
-          return resolve(stdout.trim())
-        })
-      })
-    }), {})
     console.log(chalk.gray('querying versions'))
-    const latests = await forkJoin(sources).toPromise()
+    const responses = await Promise.all(pckgs.map((pckg) => new Promise(async (resolve, reject) => {
+      try {
+        const { version = false } = await libnpm.manifest(pckg)
+        return resolve({ [pckg]: version })
+      } catch (error) {
+        if (error.statusCode === 404) {
+          return resolve({ [pckg]: false })
+        }
+        return reject(error)
+      }
+    })))
+    return resolve(responses.reduce((versions, response) => ({ ...versions, ...response }), {}))
+  } catch (error) {
+    return reject(error)
+  }
+})
+
+const run = async (options) => {
+  try {
+    console.log(chalk.green(`upgradeps v${version}`))
+    const { deps, packageJSON, packagePath, pckgs } = getInfo()
+    const versions = await queryVersions(pckgs)
+    await upgrade({ deps, options, packageJSON, packagePath, versions })
+  } catch (error) {
+    console.log(`${chalk.red(cross)} ${errorToString(error)}`)
+    process.exit(0)
+  }
+}
+
+const upgrade = ({ deps, options, packageJSON, packagePath, versions }) => new Promise(async (resolve, reject) => {
+  try {
     let hasUpdates = false
-    Object.keys(deps).map((group) => {
-      Object.keys(deps[group]).map((pckg) => {
-        if (latests[pckg]) {
+    for (const group of Object.keys(deps)) {
+      for (const pckg of Object.keys(deps[group])) {
+        if (versions[pckg]) {
           const current = deps[group][pckg].replace(/[\^~]/, '').trim()
-          const latest = latests[pckg]
+          const latest = versions[pckg]
           if (current !== latest) {
             const skips = options.skip.includes(pckg)
             if (!skips) {
@@ -57,38 +75,42 @@ const upgrade = async (options) => {
             console.log(`${chalk.cyan(pckg)} ${skips ? chalk.yellow(cross) : chalk.green(tick)} ${current} ${chalk.yellow(arrowRight)} ${latest}`)
           }
         }
-      })
-    })
+      }
+    }
     if (hasUpdates) {
-      if (packageJSON.dependencies) {
-        packageJSON.dependencies = deps.dependencies
-      }
-      if (packageJSON.devDependencies) {
-        packageJSON.devDependencies = deps.devDependencies
-      }
-      writeFileSync(packagePath, JSON.stringify(packageJSON, null, 2) + '\n', 'utf8')
-      if (existsSync(resolve(process.cwd(), 'node_modules'))) {
-        const useYarn = commandExistsSync('yarn') && !options.npm
-        console.log(chalk.gray(`running ${useYarn ? 'yarn' : 'npm install'}`))
-        execSync(useYarn ? 'yarn' : 'npm install', { stdio: [] })
-      }
+      await writePackage({ deps, packageJSON, packagePath, useYarn: commandExistsSync('yarn') && !options.npm })
       console.log(chalk.blue('package.json upgraded'))
     } else {
       console.log(chalk.blue('no updates'))
     }
+    return resolve()
   } catch (error) {
-    console.log(`${chalk.red(cross)} ${errorToString(error)}`)
-    process.exit(1)
+    return reject(error)
   }
-}
+})
+
+const writePackage = ({ deps, packageJSON, packagePath, useYarn }) => new Promise((resolve, reject) => {
+  try {
+    if (packageJSON.dependencies) {
+      packageJSON.dependencies = deps.dependencies
+    }
+    if (packageJSON.devDependencies) {
+      packageJSON.devDependencies = deps.devDependencies
+    }
+    writeFileSync(packagePath, JSON.stringify(packageJSON, null, 2) + '\n', 'utf8')
+    if (existsSync(path.resolve(process.cwd(), 'node_modules'))) {
+      console.log(chalk.gray(`running ${useYarn ? 'yarn' : 'npm install'}`))
+      execSync(useYarn ? 'yarn' : 'npm install', { stdio: [] })
+    }
+    return resolve()
+  } catch (error) {
+    return reject(error)
+  }
+})
 
 commander
   .version(version, '-v, --version')
   .option('-n, --npm', 'Force npm instead of yarn')
   .option('-s, --skip <packages>', 'Skip packages')
   .parse(process.argv)
-console.log(chalk.green(`upgradeps v${version}`))
-upgrade({
-  npm: !!commander.npm,
-  skip: (commander.skip || '').split(',')
-})
+run({ npm: !!commander.npm, skip: (commander.skip || '').split(',') })
